673bfbadcbe3234740e7f3661581315f1f847007
Brenda <80357811+Chaoticwave-boop@users.noreply.github.com>
2023-11-29 10:59:11 +0100

    added some styling to the info songs page

--- resources/views/songs/show.blade.php
+++ resources/views/songs/show.blade.php
@@ -8,15 +8,26 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <a>Name : {{$songs->name}}</a> 
-                    <br>
-                    <a>Duration : {{$songs->duration}} m</a> 
-                    <br>
-                    <a>Description : {{$songs->description}}</a> 
+            <div class="py-3 sm:max-w-xl sm:mx-auto ">
+                <div class="bg-white shadow-lg border-gray-100 max-h-80	 border sm:rounded-3xl p-8 flex space-x-8">
+                    <div class="h-48 overflow-visible w-1/2">
+                        <img class="rounded-3xl shadow-lg"
+                            src="https://upload.wikimedia.org/wikipedia/en/c/ca/Tycho_-_Awake.png" alt="">
+                    </div>
+                    <div class="flex flex-col w-1/2 space-y-4">
+                        <div class="flex justify-between items-start">
+                            <h2 class="text-3xl font-bold">{{$songs->name}}</h2>
+                            <div class="bg-yellow-400 font-bold rounded-xl p-2">{{$songs->duration}} m</div>
+                        </div><p>{{$songs->description}}</p>
+                    </div>
+
                 </div>
             </div>
+
         </div>
+    </div>
+
+
+
     </div>
 </x-app-layout>
