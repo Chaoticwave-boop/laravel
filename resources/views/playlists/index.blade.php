920d732ac63961eccd55c1dc396b3efadb6a58c0
Brenda <80357811+Chaoticwave-boop@users.noreply.github.com>
2023-12-15 10:15:39 +0100

    added styling to the delete button

--- resources/views/playlists/index.blade.php
+++ resources/views/playlists/index.blade.php
@@ -22,7 +22,7 @@
                     <form action="{{ route('playlists.destroy', ['playlist' => $playlist->id]) }}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                        <button type="submit" style="display: block; margin-left: auto;" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
                             Delete
                         </button>
                     </form>
