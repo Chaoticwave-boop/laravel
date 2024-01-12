e96fd736b1f1ecf3063f7c8706568474bdb4f8bf
Brenda <80357811+Chaoticwave-boop@users.noreply.github.com>
2024-01-12 10:25:43 +0100

    added some styling to the show

--- resources/views/playlists/show.blade.php
+++ resources/views/playlists/show.blade.php
@@ -8,12 +8,23 @@
     </x-slot>
 
     <div class="py-12">
-        <h3>{{ $playlist->name }}</h3>
-
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h3>Playlist: {{ $playlist->name }}</h3>
+                </div>
+            </div>
+        </div>
         <ul>
-            @foreach ($songs as $song)
-                <li>{{ $song->name }}</li>
-            @endforeach
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        @foreach ($songs as $song)
+                            <li>{{ $song->name }}</li>
+                            @endforeach
+                    </div>
+            </div>
+        </div>
         </ul>
 
     </div>
